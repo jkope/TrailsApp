114f7052d424e02eefdddb27df18ae812aa8d4b9
Jake K <jake.kopenhefer@gmail.com>
2019-02-13 16:49:47 -0700

    get trails for search page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrailsApiService } from '../api/trails-api.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+ private trails$;
 
-  constructor() { }
+  constructor(
+    private trails: TrailsApiService
+    ) { }
 
   ngOnInit() {
   }
+
+
+  trailsByGeoLoc() {
+    // get user current lat and lon and use in the line below
+    this.trails$ = this.trails.getTrails(40.3769, -111.789);
+
+  }
+
+
+
 
 }
