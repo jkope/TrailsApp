f54d516cc50214b40d71219a9af04df8eb8c1c0c
Jake K <jake.kopenhefer@gmail.com>
2019-03-11 10:11:23 -0600

    add hiked trail and show on profile

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../api/auth.service';
+import { FirebaseService } from '../api/firebase.service';
+import { Trail } from '../shared/trail';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +11,19 @@ import { AuthService } from '../api/auth.service';
 })
 export class UserPage implements OnInit {
 private user$;
+private hiked$: Observable <Trail[]>;
 
   constructor(
     private auth: AuthService,
+    private firebase: FirebaseService
   ) { }
 
   ngOnInit() {
     this.user$ = this.auth.authenticated();
+    // this.hiked$ = this.firebase.getHasHiked();
+    this.firebase.getHasHiked().subscribe(x => {
+      console.log(x);
+    });
   }
 
   login() {
